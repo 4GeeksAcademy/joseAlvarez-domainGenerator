c85aebc9188c8033799d652a2bed3f2c3e5fecc4
Jose1gnacio <104277952+Jose1gnacio@users.noreply.github.com>
2023-06-19 14:29:42 +0000

    2

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let p = document.getElementById("generator");
+  p.innerHTML = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        //console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        let domain = document.createElement("li");
+        domain.innerHTML = pronoun[i] + adj[j] + noun[k] + ".com";
+        p.appendChild(domain);
+      }
+    }
+  }
 };
